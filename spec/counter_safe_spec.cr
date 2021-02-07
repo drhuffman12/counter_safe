275ea8a3d6b4ee4dd860aa94c87fac6d61034fe9
Daniel Huffman <drhuffman12@gmail.com>
2021-02-07 18:56:40 -0500

    drhuffman12 initial commit

---
+++ spec/counter_safe_spec.cr
@@ -0,0 +1,9 @@
+require "./spectator_helper"
+
+describe CounterSafe do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/counter_safe.cr
@@ -0,0 +1,8 @@
+# require "./counter/*"
+
+# TODO: Write documentation for `CounterSafe`
+module CounterSafe
+  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+
+  # TODO: Put your code here
+end
